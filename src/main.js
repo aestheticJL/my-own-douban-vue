909f7862efce75544930f43feb7d579e18ba3ac9
aestheticJL <m258789@126.com>
2020-04-08 00:25:54 +0800

    :sparkles:绘制了一个简单的作品详情页面

--- src/main.js
+++ src/main.js
@@ -2,11 +2,27 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.config.productionTip = false
+import {postKeyValueRequest} from "./utils/api";
+import {postRequest} from "./utils/api";
+import {putRequest} from "./utils/api";
+import {getRequest} from "./utils/api";
+import {deleteRequest} from "./utils/api";
+
+Vue.prototype.postKeyValueRequest = postKeyValueRequest;
+Vue.prototype.postRequest = postRequest;
+Vue.prototype.putRequest = putRequest;
+Vue.prototype.getRequest = getRequest;
+Vue.prototype.deleteRequest = deleteRequest;
+
+Vue.config.productionTip = false;
+
+Vue.use(ElementUI,{size:'small'});
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
